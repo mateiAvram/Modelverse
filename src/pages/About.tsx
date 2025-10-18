79c42c83a60d28ec3168ffb25bae19129edfb2bd
mateiAvram <matei.avram01@gmail.com>
2025-10-18 09:43:59 +0200

    (fix): Contact page containers now have the same  height

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -41,7 +41,7 @@ export function About() {
       {/* Hero */}
       <Card className="rounded-none border-0 bg-gray-200 xl:px-32 2xl:px-64">
         <CardContent>
-          <div className="flex flex-row flex-wrap justify-center-safe gap-8">
+          <div className="flex flex-row flex-wrap justify-between gap-8">
             <Card className="w-full border-0 bg-white lg:basis-30/100">
               <CardContent>
                 <div className="flex flex-col gap-4">
