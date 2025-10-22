5b79836a7453537d4b8342cb974acba7d11fae54
mateiAvram <matei.avram01@gmail.com>
2025-10-22 11:03:23 +0200

    (feat): Finished cases page

--- src/pages/Cases.tsx
+++ src/pages/Cases.tsx
@@ -1,4 +1,6 @@
 import YouTubeEmbed from '@/components/app/embed/youtubeEmbed';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { useInView } from 'react-intersection-observer';
 import { Banner } from '@/components/app/misc/banner';
 import infoFile from '@/data/cases.yaml?raw';
 import { Layout } from '@/components/layout';
@@ -25,6 +27,7 @@ type Case = {
 export function Cases() {
   const cases = parse(infoFile);
   const [extendedCard, setExtenedCard] = useState(false);
+
   const handleCardBtn = () => {
     if (extendedCard) {
       setExtenedCard(false);
@@ -33,7 +36,45 @@ export function Cases() {
     }
   };
 
-  console.log(cases);
+  type CaseComponentProps = {
+    item: Case;
+    index: number;
+    length: number;
+    extendedCard?: boolean; // if you are using it in your component
+  };
+
+  const CaseComponent: React.FC<CaseComponentProps> = ({
+    item,
+    index,
+    length,
+    extendedCard,
+  }) => {
+    const { ref, inView, entry } = useInView({
+      threshold: 0.8,
+    });
+
+    return (
+      <div
+        className={`flex flex-col shrink-0 grow snap-center snap-always w-48 gap-2 overflow-y-scroll transition-transform duration-200 ${inView ? 'scale-100 opacity-100' : 'scale-75 opacity-50'}`}
+        key={index}
+        ref={ref}
+      >
+        <img
+          src={`${import.meta.env.BASE_URL}${item.logo}`}
+          alt={item.name}
+          className="object-scale-down h-12 sticky"
+        />
+        <ScrollArea
+          className={`px-4 transition-card-height text-sm ${!extendedCard ? 'h-0' : 'h-48'}`}
+        >
+          {item.about}
+          <br />
+          <br />
+          {item.case}
+        </ScrollArea>
+      </div>
+    );
+  };
 
   return (
     <Layout cases={true} banner={casesBanner}>
@@ -57,70 +98,126 @@ export function Cases() {
               />
             </svg>
           </button>
-          <div className="flex flex-row overflow-x-auto scrollbar-hide w-full h-full items-center-safe snap-x snap-mandatory">
-            {cases.map((item: Case, index: number) => (
-              <div className="shrink-0 w-full grow px-16" key={index}>
+          <div className="flex flex-col items-center gap-4 w-full">
+            <div className="w-full flex flex-row overflow-x-scroll scrollbar-hide scroll-smooth snap-x snap-proximity justify-center-safe grow gap-4 px-32">
+              {cases.map((item: Case, index: number) => (
+                <CaseComponent
+                  key={index}
+                  item={item}
+                  index={index}
+                  length={cases.length}
+                  extendedCard={extendedCard}
+                />
+              ))}
+            </div>
+            <button
+              onClick={handleCardBtn}
+              className="flex flex-row w-fit text-black/70 hover:text-black bg-clip-text cursor-pointer outline-0"
+            >
+              {!extendedCard ? (
+                <>
+                  <span>Show more</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="size-6 ml-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                </>
+              ) : (
+                <>
+                  <span>Show less</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="size-6 ml-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                    />
+                  </svg>
+                </>
+              )}
+            </button>
+          </div>
+          {/* <div className="flex flex-col">
+            <div className="flex flex-row overflow-x-auto scrollbar-hide w-full h-full items-center-safe snap-x snap-mandatory gap-8">
+              {cases.map((item: Case, index: number) => (
                 <div
-                  className="flex flex-col items-center-safe shrink-0 w-full p-8 rounded-lg grow snap-center bg-gradient-to-br from-green-500/20 to-teal-500/20 gap-4"
+                  className={`flex flex-col items-center-safe shrink-0 w-64 grow snap-center gap-4 ${index == 0 ? 'ms-16' : ''} ${index == cases.length - 1 ? 'me-16' : ''}`}
                   key={index}
                 >
                   <img
-                    src={item.logo}
+                    src={`${import.meta.env.BASE_URL}${item.logo}`}
                     alt={item.name}
                     className="object-scale-down h-16"
                   />
                   <div
-                    className={`flex flex-col justify-center gap-4 text-sm transition-card-height overflow-hidden ${!extendedCard ? 'h-0' : 'h-64'}`}
+                    className={`flex flex-col gap-4 text-sm transition-card-height overflow-hidden ${!extendedCard ? 'h-0' : 'h-96'}`}
                   >
                     <p>{item.about}</p>
                     <p>{item.case}</p>
                   </div>
-                  <button
-                    onClick={handleCardBtn}
-                    className="flex flex-row bg-white rounded-full py-2 px-4 text-white font-bold bg-gradient-to-br from-green-500 to-teal-500 w-36 justify-between"
-                  >
-                    {!extendedCard ? (
-                      <>
-                        <span>See more</span>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={2}
-                          stroke="currentColor"
-                          className="size-6 ml-2"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                          />
-                        </svg>
-                      </>
-                    ) : (
-                      <>
-                        <span>See less</span>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={2}
-                          stroke="currentColor"
-                          className="size-6 ml-2"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m4.5 15.75 7.5-7.5 7.5 7.5"
-                          />
-                        </svg>
-                      </>
-                    )}
-                  </button>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+            <button
+              onClick={handleCardBtn}
+              className="flex flex-row w-30 text-black/70 hover:text-black justify-between bg-clip-text cursor-pointer"
+            >
+              {!extendedCard ? (
+                <>
+                  <span>See more</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="size-6 ml-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                </>
+              ) : (
+                <>
+                  <span>See less</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="size-6 ml-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                    />
+                  </svg>
+                </>
+              )}
+            </button>
+          </div> */}
+
           <button className="hidden rounded-full self-start mt-16">
             <svg
               xmlns="http://www.w3.org/2000/svg"
